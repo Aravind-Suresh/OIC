11f6240b47e82a63a2ed77100308722e5725ac9c
aravind <arvindsuresh2009@gmail.com>
2015-06-01 01:58:40 +0530

    added code fr background sub

--- py scripts/oic1.py
+++ py scripts/oic1.py
@@ -2,7 +2,8 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 cas = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_eye.xml')
-roi = []
+
+lapl2 = None
 
 while(1):
 	ret, img = cap.read()
@@ -11,11 +12,18 @@ while(1):
 	for (a,b,c,d) in cas.detectMultiScale(img, 1.3, 5):
 		cv2.rectangle(img, (a,b), (a+c,b+d), (255,255,255))
 		roi = img[b:b+d, a:a+c]
-		lapl = cv2.Laplacian(roi, cv2.CV_8UC1, ksize=7)
+		lapl1 = cv2.Laplacian(roi, cv2.CV_8UC1, ksize=7)
 
+		if lapl2 is not None:
+			lapl = lapl1 - lapl2
+		else :
+			lapl = lapl1
 		#cv2.imshow("roi" + str(i), roi)
 		cv2.imshow("lapl" + str(i), lapl)
 		i = i + 1
+
+		if lapl2 is None:
+			lapl2 = lapl1
 
 	cv2.imshow("img1", img)
 
