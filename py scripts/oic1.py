e80a64fbf6ccdfcac96431873e0565d12e385817
aravind <arvindsuresh2009@gmail.com>
2015-06-01 01:28:12 +0530

    small change

--- py scripts/oic1.py
+++ py scripts/oic1.py
@@ -6,14 +6,20 @@ roi = []
 
 while(1):
 	ret, img = cap.read()
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	i = 0
 	for (a,b,c,d) in cas.detectMultiScale(img, 1.3, 5):
 		cv2.rectangle(img, (a,b), (a+c,b+d), (255,255,255))
 		roi = img[b:b+d, a:a+c]
-		cv2.imshow("roi" + str(i), roi)
+		lapl = cv2.Laplacian(roi, cv2.CV_8UC1, ksize=7)
+
+		#cv2.imshow("roi" + str(i), roi)
+		cv2.imshow("lapl" + str(i), lapl)
 		i = i + 1
 
 	cv2.imshow("img1", img)
 
 	if cv2.waitKey(1) == 27:
 		break
+
+cap.release()
