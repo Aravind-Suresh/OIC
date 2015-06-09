130635fef88a1a003c62375bf897a6d0e1817cf3
mrupeshkumar <dmrupeshkumar@gmail.com>
2015-06-10 01:12:31 +0530

    harris

--- py scripts/oic0.py
+++ py scripts/oic0.py
@@ -10,11 +10,18 @@ while(1):
 	for (a,b,c,d) in cas.detectMultiScale(img, 1.3, 5):
 		cv2.rectangle(img, (a,b), (a+c,b+d), (255,255,255))
 		roi = img[b:b+d, a:a+c]
+		roi_corner=roi
 		lapl = cv2.Laplacian(roi, cv2.CV_8UC1, ksize=7)
 		edge = cv2.Canny(roi, 25, 25)
+		gray = np.float32(roi)
+		dst = cv2.cornerHarris(gray,2,3,0.04)
+		dst = cv2.dilate(dst,None)
+		roi_corner[dst>0.01*dst.max()]=[0,0,255]
 		#cv2.imshow("roi" + str(i), roi)
-		cv2.imshow("lapl" + str(i), lapl)
-		cv2.imshow("edge" + str(i), edge)
+		if i<3:
+			cv2.imshow("lapl" + str(i), lapl)
+			cv2.imshow("corner" + str(i), roi_corner)
+			cv2.imshow("edge" + str(i), edge)
 		i = i + 1
 
 	cv2.imshow("img1", img)
